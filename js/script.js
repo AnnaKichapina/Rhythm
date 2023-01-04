b9d003deaee3aa5f4adfa2306c37126389b4e5de
AnnaKuchapina <eternity.anna@gmail.com>
2023-01-05 00:56:07 +0200

    all about slider

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ const reviewsSwiper = document.querySelector('.swiper-reviews');
 if (reviewsSwiper) {
 const swiper = new Swiper('.swiper-reviews', {
   // Optional parameters
+  autoHeight: true,
   loop: true,
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 });
 }
